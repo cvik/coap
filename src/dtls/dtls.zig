a24b9053cab07639b720262b18869770953a3f88
Christoffer Vikström <cvi@heya.tech>
2026-03-12 11:19:08 +0100

    feat: DTLS record layer with anti-replay

--- src/dtls/dtls.zig
+++ src/dtls/dtls.zig
@@ -5,10 +5,12 @@ pub const ContentType = types.ContentType;
 pub const Ccm = @import("Ccm.zig");
 pub const Prf = @import("Prf.zig");
 pub const Cookie = @import("Cookie.zig");
+pub const Record = @import("Record.zig");
 
 test {
     _ = types;
     _ = Ccm;
     _ = Prf;
     _ = Cookie;
+    _ = Record;
 }
